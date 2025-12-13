48e7659d2a3b88e403d98da51664db486f330b9d
Tiesstes <flircio@gmail.com>
2025-12-13 02:21:53 +0100

    changed dir constant, added simple tests for the network

--- project/preprocess.py
+++ project/preprocess.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import torch
 import torchaudio
@@ -9,6 +10,7 @@ from dataset import SpeechCommandsKWS
 
 from project.model.arch.blocks import BCResBlock
 from project.model.arch.blocks import ConvBNReLU
+from project.model.kws_net import KWSNet
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -17,75 +19,53 @@ print(torchaudio.__version__)
 
 
 # konfiguracja
-DATASET_DIR = os.path.abspath(os.getcwd())
+BASE_DATASET_DIR = os.path.abspath(os.getcwd())
+NOISE_PATH = Path(BASE_DATASET_DIR) / "SpeechCommands" / "speech_commands_v0.02" / "_background_noise_"
 BATCH_SIZE = 32
 
-print(DATASET_DIR)
+print("Ścieżka, w której znajduje się katalog pobranego bazowego dataset'u:" , BASE_DATASET_DIR)
 # podzbiory:
-train_base = SPEECHCOMMANDS(root=DATASET_DIR, subset="training")
+train_base = SPEECHCOMMANDS(root=BASE_DATASET_DIR, subset="training")
 #val_base   = SPEECHCOMMANDS(root=dataset_dir, subset="validation")
 #test_base  = SPEECHCOMMANDS(root=dataset_dir, subset="testing")
 
-print(train_base.__len__())
-
-training = SpeechCommandsKWS(train_base)
-
-print(training.__len__())
-print(training.speaker_counter)
-print(training.label_mapping)
-
-"""
 # datasety prawdziwe:
-train_dataset = SpeechCommandsKWS(train_base, label_mapping, speaker_mapping)
+train_dataset = SpeechCommandsKWS(train_base, NOISE_PATH)
 #val_dataset  = SpeechCommandsKWS(val_base,   label_mapping, speaker_mapping)
 #test_dataset  = SpeechCommandsKWS(test_base,  label_mapping, speaker_mapping)
 
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
 
-batch = next(iter(train_loader)) # wzięcie pierwszego batcha z dataloadera (a dataset zwraca klucze i ich wartości)
-x = batch["log_mel_spectrogram"]   # [Batch, 1, 40, ilość ramek czasowych (1s: 30ms okno i 10ms przesunięcie, czyli 0-30ms potem 10-40ms...)]
-y = batch["label"]                 # [Batch], bo tyle etykiet w batchu
-s_id = batch["speaker_id"]            # [Batch], bo tyle mówców w batchu
+model = KWSNet(12, train_dataset.speaker_counter.__len__())
+model = model.to(device)
 
-print(x.shape, y.shape, s_id.shape)
+def check_forward(data_loader, model):
+    model.eval()
+    batch = next(iter(data_loader))
+
+    x = batch["log_mel_spectrogram"].to(device)   # Tensor: [B, 1, F, T] u Ciebie
+    spk = batch["speaker_id"].to(device)           # LongTensor: [B]
+
+    with torch.no_grad():
+        y = model(x, spk)
+
+    assert list(y.shape) == [x.shape[0], 12]  # [B, num_classes]
+    
+def check_backward(data_loader, model):
+    model.train()
+    batch = next(iter(data_loader))
+
+    x = batch["log_mel_spectrogram"].to(device)
+    spkr_id = batch["speaker_id"].to(device)
+    y_true = batch["label"].to(device)             # [B]
+
+    logits = model(x, spkr_id)
+    loss = torch.nn.CrossEntropyLoss()(logits, y_true)
+    loss.backward()
+
+    assert model.speaker_embedding.weight.grad is not None
 
 
-
-B, C_in, F, T = x.shape
-
-print("wejście do bloku:", x.shape)"""
-"""
-# mały teścik bloków
-
-#blok conv
-convBNReLU = ConvBNReLU(
-    in_channels=C_in,
-    out_channels=C_in,
-    kernel_size=3,
-    stride=1,
-    padding=1,
-)
-out = convBNReLU(x) # to samo co convBNReLU.forward(x)
-print("wyjście z bloku ConvBNReLU:", out.shape)
-
-# blok normal
-normal_block = BCResBlock(
-    in_channels=C_in,
-    out_channels=C_in,
-    ssn_subbands=4,
-    dropout_rate=0.1,
-    is_transition=False,
-)
-# blok transition
-transition_block = BCResBlock(
-    in_channels=C_in,
-    out_channels=C_in + 10,
-    ssn_subbands=4,
-    dropout_rate=0.1,
-    is_transition=True,
-)
-
-out = normal_block(x)
-print("wyjście z bloku normal:", out.shape)
-out = transition_block(out)
-print("wyjście z bloku transition:", out.shape)"""
+if __name__ == "__main__":
+    check_forward(train_loader, model)
+    check_backward(train_loader, model)
