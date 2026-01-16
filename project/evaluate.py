45e12cf0be1918f0d4c080725a5df2bf524df6ff
Tiesstes <flircio@gmail.com>
2026-01-16 18:35:19 +0100

    evaluation works

--- project/evaluate.py
+++ project/evaluate.py
@@ -6,11 +6,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from torchaudio.datasets import SPEECHCOMMANDS
 
-from project.data.dataset import SpeechCommandsKWS
+from project.data.dataset import CustomWAVSpeechCommandsKWS, split_custom_data
 from project.model.kws_net import KWSNet
 
 import warnings
-
 import os
 
 warnings.filterwarnings("ignore", category=UserWarning, module="torchaudio")
@@ -18,13 +17,18 @@ warnings.filterwarnings("ignore", category=UserWarning, module="torchaudio")
 # konfiguracje
 BASE_PATH = Path(__file__).resolve().parent
 GSC_PATH = BASE_PATH / "data"
+
+CUSTOM_DATA_ROOT = GSC_PATH / "SpeechCommands" / "custom_speech_commands_v0.02"
 NOISE_PATH = GSC_PATH / "SpeechCommands" / "speech_commands_v0.02" / "_background_noise_"
 DATASPLIT_MANIFEST_PATH = BASE_PATH / "data" / "splits" / "experiment_v1.pt"
 
-PRETRAIN_CHECKPOINT = Path(BASE_PATH) / "model" / "pretrain_checkpoint.pt"
-FINETUNE_CHECKPOINT = Path(BASE_PATH) / "model" / "finetune_checkpoint.pt"
+IMPORT_EPOCH_FINETUNE = 36
+IMPORT_EPOCH_PRETRAIN = 30
 
-BATCH_SIZE = 128
+PRETRAIN_IMPORT_CHECKPOINT = Path(BASE_PATH) / "model" / f"pretrain_checkpoint_{IMPORT_EPOCH_PRETRAIN}.pt"
+FINETUNE_IMPORT_CHECKPOINT = Path(BASE_PATH) / "model" / f"finetune_checkpoint_{IMPORT_EPOCH_FINETUNE}.pt"
+
+BATCH_SIZE = 64
 WORKERS = 0
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -35,9 +39,12 @@ print("Torch version:", torch.__version__)
 print()
 
 
-
 def evaluate(model, data_loader, device):
-    """Ewaluuj model na zbiorze testowym, zwracaj predykcje i etykiety."""
+    """
+    Ewaluuj model na zbiorze testowym, zwracaj predykcje i etykiety
+    Tutaj jest ewaluacja pretrain ignoruje speaker embedding zupełnie
+    """
+
     model.eval()
     all_predictions, all_labels = [], []
     total_correct, total_n = 0, 0
@@ -67,14 +74,15 @@ def compute_metrics(all_predictions, all_labels, model_class_names):
     precision = precision_score(all_labels, all_predictions, average=None, zero_division=0)
     recall = recall_score(all_labels, all_predictions, average=None, zero_division=0)
 
-    print(f"Precision (macro): {precision_score(all_labels, all_predictions, average='macro', zero_division=0):.4f}")
-    print(f"Recall (macro):    {recall_score(all_labels, all_predictions, average='macro', zero_division=0):.4f}\n")
+    print(f"Precision (macro): {precision_score(all_labels, all_predictions, labels=list(range(len(model_class_names) - 1)),average='macro', zero_division=0):.4f}")
+    print(f"Recall (macro):    {recall_score(all_labels, all_predictions, labels=list(range(len(model_class_names) - 1)) ,average='macro', zero_division=0):.4f}\n")
 
     for i, class_name in enumerate(model_class_names):
-
+        if class_name == "silence":
+            continue
         print(f"{class_name:15s} | Precision: {precision[i]:.4f} | Recall: {recall[i]:.4f}")
 
-    return confusion_matrix(all_labels, all_predictions), precision, recall
+    return confusion_matrix(all_labels, all_predictions, labels=list(range(len(model_class_names) - 1))), precision, recall
 
 
 def plot_confusion_matrix(cm, model_class_names, name, save_path=None):
@@ -107,15 +115,15 @@ if __name__ == "__main__":
     if not DATASPLIT_MANIFEST_PATH.exists():
         raise FileNotFoundError(f"Nie znaleziono manifestu: {DATASPLIT_MANIFEST_PATH}")
 
-    dataset_indexer = torch.load(DATASPLIT_MANIFEST_PATH, map_location=DEVICE)
-    finetune_split = dataset_indexer["indices"]["finetune"]  # Zachowujemy Twoją nazwę zmiennej
+    split_manifest = torch.load(DATASPLIT_MANIFEST_PATH, map_location=DEVICE)
+    finetune_split = split_manifest["splits"]["finetune"]  # zmienna z manifestu
 
     # pretrain checkpoint
     print("Ładuję pretrain checkpoint...")
-    if not PRETRAIN_CHECKPOINT.exists():
-        raise FileNotFoundError(f"Nie znaleziono: {PRETRAIN_CHECKPOINT}")
+    if not PRETRAIN_IMPORT_CHECKPOINT.exists():
+        raise FileNotFoundError(f"Nie znaleziono: {PRETRAIN_IMPORT_CHECKPOINT}")
 
-    pretrain_checkpoint = torch.load(PRETRAIN_CHECKPOINT, map_location=DEVICE)
+    pretrain_checkpoint = torch.load(PRETRAIN_IMPORT_CHECKPOINT, map_location=DEVICE)
     old_speaker_id_map = pretrain_checkpoint["speaker_id_map"]
     old_label_map = pretrain_checkpoint["label_map"]
 
@@ -127,10 +135,10 @@ if __name__ == "__main__":
 
 
     # finetune checkpoint (jeśli istnieje)
-    if FINETUNE_CHECKPOINT.exists():
+    if FINETUNE_IMPORT_CHECKPOINT.exists():
 
         print("Ładuję finetune checkpoint...")
-        finetune_checkpoint = torch.load(FINETUNE_CHECKPOINT, map_location=DEVICE)
+        finetune_checkpoint = torch.load(FINETUNE_IMPORT_CHECKPOINT, map_location=DEVICE)
         new_speaker_id_map = finetune_checkpoint["speaker_id_map"]
         has_finetune = True
 
@@ -144,27 +152,27 @@ if __name__ == "__main__":
 
     # dataset testowy
     print("Tworzę test dataset...")
-
+    _, _, test_indices = split_custom_data(CUSTOM_DATA_ROOT, seed=1234)
     # pełna mapa finetune
-    test_dataset = SpeechCommandsKWS(dataset=base_data, split_indices=finetune_split["test"],
-                                     speaker_id_map=dataset_indexer["maps"]["speaker_id_map_finetune"],
-                                     noise_dir=NOISE_PATH, deterministic=True)
+    test_dataset = CustomWAVSpeechCommandsKWS(root_data_dir=CUSTOM_DATA_ROOT,split_indices=test_indices,
+                                              speaker_id_map=new_speaker_id_map, label_map=split_manifest["maps"]["label_map"],
+                                              deterministic=True)
 
     test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=WORKERS)
 
-    print(f"test_ds: {len(test_dataset)}")
+    print(f"Dłgiść datasetu treningowego: {len(test_dataset)}")
     print("")
 
     # Ewaluacja PRETRAINU
-    print("=" * 60)
+    print("~~" * 100)
     print("PRETRAIN")
-    print("=" * 60)
+    print("~~" * 100)
 
     num_speakers_pretrain = len(old_speaker_id_map)
     num_classes = len(old_label_map)
 
     model_pretrain = KWSNet(num_of_classes=num_classes, num_of_speakers=num_speakers_pretrain).to(DEVICE)
-    model_pretrain.load_state_dict(pretrain_checkpoint["model_state"])
+    model_pretrain.load_state_dict(pretrain_checkpoint["model_state"], strict=False)
 
     # oszukanie datasetu, bo został zainicjalizowany na finetune
     test_loader.dataset.speaker_id_map = old_speaker_id_map
@@ -184,9 +192,9 @@ if __name__ == "__main__":
     # FINETUNE jak jest
     if has_finetune:
 
-        print("=" * 60)
+        print("~~" * 100)
         print("FINETUNE")
-        print("=" * 60)
+        print("~~" * 100)
 
         num_speakers_finetune = len(new_speaker_id_map)
 
@@ -204,9 +212,9 @@ if __name__ == "__main__":
         plot_confusion_matrix(cm_finetune, class_names, name="Finetuned", save_path=results_path / "cm_finetune.png")
 
         # porównanie
-        print("=" * 60)
-        print("PORÓWNANIE MODELU 2 FAZ")
-        print("=" * 60)
-        print(f"Accuracy improvement: {accuracy_finetune - accuracy_pretrain:+.4f}")
+        print("~~" * 100)
+        print("PORÓWNANIE Z 2 FAZ")
+        print("~~" * 100)
+        print(f"Różnica w accuracy: {accuracy_finetune - accuracy_pretrain:+.4f}")
         print(f"Pretrain:  {accuracy_pretrain:.4f}")
         print(f"Finetune:  {accuracy_finetune:.4f}")
